491e2444a6de71daf87fed3072d96e4ec6c5dbee
rosyiddwisetyawan <42093687+rosyiddwisetyawan@users.noreply.github.com>
2018-11-11 09:41:40 +0700

    Rosyid Dwi Setyawan

--- app/src/main/java/com/rds/jobs/GaleryActivity.java
+++ app/src/main/java/com/rds/jobs/GaleryActivity.java
@@ -259,58 +259,6 @@ public class GaleryActivity extends AppCompatActivity {
         shareInstance.share();
     }
 
-//    private class Description extends AsyncTask<Void, Void, Void> {
-//        String desc;
-//
-//        @Override
-//        protected void onPreExecute() {
-//            super.onPreExecute();
-//            mProgressDialog = new ProgressDialog(GaleryActivity.this);
-//            mProgressDialog.setTitle("Android Basic JSoup Tutorial");
-//            mProgressDialog.setMessage("Loading...");
-//            mProgressDialog.setIndeterminate(false);
-//            mProgressDialog.show();
-//        }
-//
-//        @Override
-//        protected Void doInBackground(Void... params) {
-//            try {
-//                // Connect to the web site
-//                Document mBlogDocument = Jsoup.connect(url).get();
-//                // Using Elements to get the Meta data
-//                Elements mElementDataSize = mBlogDocument.select("div[class=author-date]");
-//                // Locate the content attribute
-//                int mElementSize = mElementDataSize.size();
-//
-//                for (int i = 0; i < mElementSize; i++) {
-//                    Elements mElementAuthorName = mBlogDocument.select("span[class=vcard author post-author test]").select("a").eq(i);
-//                    String mAuthorName = mElementAuthorName.text();
-//
-//                    Elements mElementBlogUploadDate = mBlogDocument.select("span[class=post-date updated]").eq(i);
-//                    String mBlogUploadDate = mElementBlogUploadDate.text();
-//
-//                    Elements mElementBlogTitle = mBlogDocument.select("h2[class=entry-title]").select("a").eq(i);
-//                    String mBlogTitle = mElementBlogTitle.text();
-//
-//                    mAuthorNameList.add(mAuthorName);
-//                    mBlogUploadDateList.add(mBlogUploadDate);
-//                    mBlogTitleList.add(mBlogTitle);
-//                }
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//            return null;
-//        }
-//
-//        @Override
-//        protected void onPostExecute(Void result) {
-//            // Set description into TextView
-//
-//
-//            mProgressDialog.dismiss();
-//        }
-//    }
-
     public String scrapping(String url){
         Document doc = Jsoup.parse(url);
         Elements ul = doc.select("ul");
